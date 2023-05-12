3c5cd78c55798dbdb5ec4d485ef90a7bc2c108d1
Shahabr99 <sean.rohani9@gmail.com>
2023-05-11 20:21:24 -0700

    Frontend is done

--- static/app.js
+++ static/app.js
@@ -1,1 +1,30 @@
+"use strict";
+const list = document.querySelector(".list");
 
+const getCupcakes = async function () {
+  const res = await axios.get("http://127.0.0.1:5000/api/cupcakes");
+
+  const data = res.data.cupcake;
+
+  for (let i = 0; i < data.length; i++) {
+    list.insertAdjacentHTML("beforeend", `<li>${data[i].flavor}</li>`);
+  }
+};
+
+getCupcakes();
+
+$(".cupcake-form").on("submit", async function (e) {
+  e.preventDefault();
+
+  const flavor = $("#flavor").val();
+  const size = document.querySelector("#size").value;
+  const rating = $("#rating").val();
+  const image = $("#image").val();
+
+  await axios.post("/api/cupcakes", {
+    flavor: flavor,
+    size: size,
+    rating: rating,
+    image: image,
+  });
+});
